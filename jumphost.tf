d9867b1f5c891c8234212db015aad11e1e4ab1f4
Eric Dahl <eric.c.dahl@gmail.com>
2021-02-28 13:25:20 -0800

    jumphost: freebsd -> amazon linux 2

--- jumphost.tf
+++ jumphost.tf
@@ -1,40 +1,13 @@
-data "aws_ami" "freebsd_11" {
-  most_recent = true
-
-  filter {
-    name = "owner-id"
-
-    values = [
-      "118940168514",
-    ]
-  }
-
-  filter {
-    name = "name"
-
-    values = [
-      "FreeBSD 11.1-STABLE-amd64*",
-    ]
-  }
+data "aws_ssm_parameter" "amazon_linux_2" {
+  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
 }
 
 resource "aws_instance" "jumphost" {
-  ami                    = data.aws_ami.freebsd_11.image_id
+  ami                    = data.aws_ssm_parameter.amazon_linux_2.value
   instance_type          = "t2.small"
   subnet_id              = module.vpc.subnet_public1
   vpc_security_group_ids = [module.vpc.sg_allow_22, module.vpc.sg_allow_egress]
   key_name               = var.key_name
-
-  user_data = <<EOF
-#!/usr/bin/env sh
-
-export ASSUME_ALWAYS_YES=YES
-
-pkg update -y
-pkg install -y bash
-chsh -s /usr/local/bin/bash ec2-user
-EOF
-
 
   tags = {
     Name = "jumphost"
