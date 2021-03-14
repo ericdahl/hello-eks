3ad5dd9028d0ac9482bf43341a66dd2fbebb5bd0
Eric Dahl <eric.c.dahl@gmail.com>
2021-03-14 14:28:33 -0700

    cleanup

--- main.tf
+++ main.tf
@@ -40,25 +40,6 @@ resource "aws_eks_cluster" "default" {
   depends_on = [aws_cloudwatch_log_group.control_plane]
 }
 
-//
-//resource "aws_eks_cluster" "cluster" {
-//  name     = var.cluster_name
-//  role_arn = aws_iam_role.cluster.arn
-//
-//  vpc_config {
-//    subnet_ids = [
-//      module.vpc.subnet_private1,
-//      module.vpc.subnet_private2,
-//      module.vpc.subnet_private3,
-//    ]
-//
-//    security_group_ids = [
-//      module.vpc.sg_allow_egress,
-//      aws_security_group.cluster.id,
-//    ]
-//  }
-//}
-//
 resource "aws_eks_node_group" "default" {
   cluster_name    = aws_eks_cluster.default.name
   node_group_name = var.name
@@ -79,28 +60,3 @@ resource "aws_eks_node_group" "default" {
     source_security_group_ids = [aws_security_group.jumphost.id]
   }
 }
-//
-//resource "aws_security_group" "cluster" {
-//  vpc_id      = module.vpc.vpc_id
-//  name        = "${var.cluster_name}_cluster"
-//  description = "Allows communication between k8s nodes"
-//
-//  tags = {
-//    Name = "${var.cluster_name}_cluster"
-//    // necessary for k8s Service to initalize
-//    KubernetesCluster = var.cluster_name
-//  }
-//}
-//
-//resource "aws_security_group_rule" "cluster_0" {
-//  security_group_id = aws_security_group.cluster.id
-//
-//  type      = "ingress"
-//  protocol  = "all"
-//  from_port = 0
-//  to_port   = 0
-//
-//  //  source_security_group_id = "${aws_security_group.cluster.id}"
-//  self = true
-//}
-//
