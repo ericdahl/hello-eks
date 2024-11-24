0917f2e4853d90f2357f211b138ccf9283554094
Eric Dahl <eric.c.dahl@gmail.com>
2024-11-24 11:15:17 -0800

    fmt

--- jumphost.tf
+++ jumphost.tf
@@ -1,50 +1,49 @@
-data "aws_ssm_parameter" "amazon_linux_2" {
- name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+data "aws_ssm_parameter" "amazon_linux_2023" {
+  name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-arm64"
 }
 
 resource "aws_key_pair" "default" {
- key_name   = var.name
- public_key = var.public_key
+  key_name   = var.name
+  public_key = var.public_key
 }
 
 resource "aws_instance" "jumphost" {
- ami           = data.aws_ssm_parameter.amazon_linux_2.value
- instance_type = "t3.micro"
- subnet_id     = aws_subnet.public["us-east-1a"].id
- vpc_security_group_ids = [
-   aws_security_group.jumphost.id
- ]
- key_name = aws_key_pair.default.key_name
+  ami           = data.aws_ssm_parameter.amazon_linux_2023.value
+  instance_type = "t4g.small"
+  subnet_id     = aws_subnet.public["us-east-1a"].id
+  vpc_security_group_ids = [
+    aws_security_group.jumphost.id
+  ]
+  key_name = aws_key_pair.default.key_name
 
- tags = {
-   Name = "${var.name}-jumphost"
- }
+  tags = {
+    Name = "${var.name}-jumphost"
+  }
 }
 
 resource "aws_security_group" "jumphost" {
- vpc_id = aws_vpc.default.id
- name   = "${var.name}-jumphost"
+  vpc_id = aws_vpc.default.id
+  name   = "${var.name}-jumphost"
 }
 
 resource "aws_security_group_rule" "jumphost_egress_all" {
- security_group_id = aws_security_group.jumphost.id
- type              = "egress"
- protocol          = "-1"
- from_port         = 0
- to_port           = 0
- cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.jumphost.id
+  type              = "egress"
+  protocol          = "-1"
+  from_port         = 0
+  to_port           = 0
+  cidr_blocks       = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group_rule" "jumphost_ingress_ssh" {
- security_group_id = aws_security_group.jumphost.id
- type              = "ingress"
- protocol          = "tcp"
- from_port         = 22
- to_port           = 22
- cidr_blocks       = [var.admin_cidr]
+  security_group_id = aws_security_group.jumphost.id
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 22
+  to_port           = 22
+  cidr_blocks       = [var.admin_cidr]
 }
 
-
 output "jumphost" {
- value = aws_instance.jumphost.public_ip
+  value = aws_instance.jumphost.public_ip
 }
