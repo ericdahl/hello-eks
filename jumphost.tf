369ec33ae12eb6c16dd90ececf322b50dbd2dcbb
Eric Dahl <eric.c.dahl@gmail.com>
2024-11-24 11:26:04 -0800

    update jumphost (not fuly working)

--- jumphost.tf
+++ jumphost.tf
@@ -2,6 +2,39 @@ data "aws_ssm_parameter" "amazon_linux_2023" {
   name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-arm64"
 }
 
+data "aws_iam_policy_document" "jumphost_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+
+    effect = "Allow"
+  }
+}
+
+resource "aws_iam_role" "jumphost_role" {
+  name               = "${var.name}-jumphost-role"
+  assume_role_policy = data.aws_iam_policy_document.jumphost_assume_role_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "jumphost_eks_access" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.jumphost_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "jumphost_eks_cni" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.jumphost_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "jumphost_eks_read_only" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.jumphost_role.name
+}
+
 resource "aws_key_pair" "default" {
   key_name   = var.name
   public_key = var.public_key
@@ -14,11 +47,26 @@ resource "aws_instance" "jumphost" {
   vpc_security_group_ids = [
     aws_security_group.jumphost.id
   ]
-  key_name = aws_key_pair.default.key_name
+  key_name             = aws_key_pair.default.key_name
+  iam_instance_profile = aws_iam_instance_profile.jumphost_profile.name
 
   tags = {
     Name = "${var.name}-jumphost"
   }
+
+  user_data = <<-EOF
+    #!/bin/bash
+    yum update -y
+    curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/arm64/kubectl"
+    chmod +x ./kubectl
+    mv ./kubectl /usr/local/bin/kubectl
+    aws eks update-kubeconfig --name ${aws_eks_cluster.default.name} --region us-east-1
+  EOF
+}
+
+resource "aws_iam_instance_profile" "jumphost_profile" {
+  name = "${var.name}-jumphost-profile"
+  role = aws_iam_role.jumphost_role.name
 }
 
 resource "aws_security_group" "jumphost" {
